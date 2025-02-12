d5b464d22cb5ae568fc5e1672d76acfa2cbdb2aa
Phil Alonso <philalonso@gmail.com>
2025-02-11 18:26:05 -0600

    Added hooks to module file

--- web/modules/custom/repo_nodes/src/RepoNodesService.php
+++ web/modules/custom/repo_nodes/src/RepoNodesService.php
@@ -4,16 +4,52 @@ declare(strict_types=1);
 
 namespace Drupal\repo_nodes;
 
+use Drupal\Core\Config\ConfigFactoryInterface;
+use Drupal\Component\Plugin\PluginManagerInterface;
+
 /**
  * Service description.
  */
 final class RepoNodesService {
+  /**
+   * Constructs a RepoNodes object.
+   *
+   * @param \Drupal\Component\Plugin\PluginManagerInterface $pluginManagerRepoNodes
+   *   The plugin manager interface.
+   * @param \Drupal\Core\Config\ConfigFactoryInterface $configFactory
+   *   The configuration factory interface.
+   */
+  public function __construct(
+    protected PluginManagerInterface $pluginManagerRepoNodes,
+    protected ConfigFactoryInterface $configFactory
+  ) {}
 
   /**
-   * Method description.
+   * Get help text from enabled plugins.
+   *
+   * @return string
+   *   Concatenated help strings.
+   * @throws \Drupal\Component\Plugin\Exception\PluginException
    */
-  public function doSomething(): void {
-    // @todo Place your code here.
+  public function getValidatorHelpText(): string {
+    $repository_plugins = [];
+    // Get the enabled list of our plugins from the Drupal config system.
+    // Use the Null Coalesce Operator in case no repos are enabled.
+    $repository_plugin_ids = $this->configFactory->get('repo_nodes.settings')->get('repository_plugins') ?? [];
+    foreach ($repository_plugin_ids as $repository_plugin_id) {
+      if (!empty($repository_plugin_id)) {
+        $repository_plugins[] = $this->pluginManagerRepoNodes->createInstance($repository_plugin_id);
+      }
+    }
+
+    $help = [];
+
+    /** @var \Drupal\repo_nodes\RepoNodes\RepoNodesInterface $repository_plugin */
+    foreach ($repository_plugins as $repository_plugin) {
+      $help[] = $repository_plugin->validateHelpText();
+    }
+
+    return implode(' ', $help);
   }
 
 }
